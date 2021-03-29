3b337008848946fdadd66150f3c583e8ef389c1a
Ron Rodriguez <ron_rodriguez@grainwaves.ca>
2021-03-28 18:21:19 -0600

    Add a to string method to FurnitureOrder and User Interaction to SupplyChainManager

--- edu/ucalgary/ensf409/FurnitureOrder.java
+++ edu/ucalgary/ensf409/FurnitureOrder.java
@@ -3,8 +3,8 @@
 // Team: ENSF409 Group 48
 // Author: Ron Rodriguez
 // Creation Date: March 27, 2021
-// Version: 0.001
-// Revision Date: March 27, 2021
+// Version: 0.02
+// Revision Date: March 28, 2021
 //
 // Description: A class containing parameters of a furniture order
 /////////////////////////////////////////////////////////////////
@@ -12,10 +12,14 @@ package edu.ucalgary.ensf409;
 
 /**
  * @author <a href ="mailto:zarodrig@ucalgary.ca>Zorondras Rodriguez</a> 
- * @version 0.002 03/20/2021
- * @since 0.001 03/19/2021
+ * @version 0.02 03/28/2021
+ * @since 0.01 03/27/2021
  */
 
+ /**
+  * FurnitureOrder is a class the contains the values of a furniture order
+  * Attributes: (String)category, (String)type, (int)quantity
+  */
  public class FurnitureOrder{
 
  ////////////////////////////// ATTRIBUTES ////////////////////////////
@@ -63,5 +67,28 @@ private  int quantity;
 
 
  /////////////////////////// OTHER //////////////////////////////////////
+
+    /**
+     * toString() method
+     * @return (String) a representation of the attributes in the object 
+     */
+    public String toString(){
+
+        String output="";
+
+        output+="Category: " + this.category +"\n";
+        output+="Type: " + this.type +"\n";
+        output+="Quantity" + this.quantity +"\n";
+
+        return output;
+    }
+
+    /**
+     * print method prints the object attributes
+     */
+    public void print(){
+        System.out.println(this.toString());
+    }
+
 
  }// closing brace of class FurnitureOrder
---
+++ edu/ucalgary/ensf409/SupplyChainManager.java
@@ -0,0 +1,125 @@
+//////////////////////////////
+// Title: SupplyChainManager
+// Author: (Ron) Zorondras Rodriguez
+// Creation Date: March 28, 2021
+// Version: 0.01
+// Revision Date: March 28, 2021
+//
+// Description: Main control unit
+///////////////////////////////////////////////////
+
+package edu.ucalgary.ensf409;
+import java.sql.*;
+import java.lang.IllegalArgumentException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * @author <a href ="mailto:zarodrig@ucalgary.ca>Zorondras Rodriguez</a> 
+ * @version 0.02 03/28/2021
+ * @since 0.01 03/28/2021
+ */
+
+/**
+ * SupplyChainManager is a class to get user input for a request
+ * to an Inventory system using MySQL.  The user makes a request
+ * The request is parsed, and a combination of lowest cost from the inventory is created
+ * if such a combination does not exist, a helpful output is made to the console, suggesting 
+ * relevant manufacturers.  If such a cheapest combination exists, then an orderform is created
+ * and writen to disk.  Simultaneously the requested items are deleted from the database.
+ */
+public class SupplyChainManager{
+
+    ////////////////////  ATTRIBUTES ///////////////////////////////
+    // change these variables for your local installation
+    private String username ="Marasco"; 
+    private String password = "ensf409";
+    private String dbURL="jdbc:mysql://localhost/INVENTORY";
+    
+    // Make these private and add setters and getters later
+
+    // a DataBaseManager object 
+    public DataBaseManager dBM;  
+
+    // a scanner for user interaction  
+    public Scanner keyconsole; 
+
+    // a furniture order object
+    public FurnitureOrder userOrder;
+
+    ///////////////////// CONSTRUCTOR //////////////////////////////////
+    public SupplyChainManager(){
+    // construct a new Registration object to manage INVENTORY
+    this.dBM = new DataBaseManager(this.dbURL,this.username,this.password);
+
+    }
+   
+    ////////////////////// METHODS  /////////////////////////////////////
+
+    /**
+     * getUserOrder is a user interaction method to populate the userOrder FurnitureOrder object
+     */
+    public void getUserOrder(){
+        String userResponseStr,category,type;
+        int userResponseInt,quantity;
+
+        // make a keyboard scanner on System.in
+        keyconsole = new Scanner(System.in);
+
+        System.out.println("Welcome to the SupplyChainManager Program!");
+        System.out.println("___________________________________________");
+        System.out.println("What category of object would you like?:");
+        printCategories();
+        userResponseStr=keyconsole.nextLine();
+        userResponseInt = Integer.parseInt(userResponseStr);
+        while (userResponseInt < 1 || userResponseInt > 4){
+            System.out.println("Error: Selection out of range, try again!");
+            userResponseStr=keyconsole.nextLine();
+            userResponseInt = Integer.parseInt(userResponseStr);
+        }
+
+        System.out.println("The user selected: " + userResponseStr);
+
+        return;
+    }
+
+    /**
+     * print categories prints option choices for the 
+     * categories from DB INVENTORY.  These should be constant
+     */
+    public void printCategories(){
+        System.out.println("1. CHAIR ");
+        System.out.println("2. DESK ");
+        System.out.println("3. FILING ");
+        System.out.println("4. LAMP ");
+        System.out.println("___________________________________________");
+        return;
+    }
+
+    /////////////////////////////////////////////////////////////////////
+    /**
+     * main function for running the program 
+     * @param args (String[]) console arguments (optional)
+     */
+    public static void main(String[] args){
+
+        System.out.println("SupplyChainManager");
+        // make a new SCM object 
+        SupplyChainManager SCM = new SupplyChainManager();
+        // initialize the database
+        SCM.dBM.initializeConnection();
+
+        // Need a menu for quit, order, etc
+        // SCM.userMenu();
+
+        // get some user interaction for an order 
+        SCM.getUserOrder(); 
+
+
+
+        // close the conenection 
+        SCM.dBM.closeDBConnection();
+    }
+
+
+}
