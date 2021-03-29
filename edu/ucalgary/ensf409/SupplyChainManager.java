4dd56f48647a1902fadb83efb2dfb5b0a22d0431
Ron Rodriguez <ron_rodriguez@grainwaves.ca>
2021-03-29 08:51:06 -0600

    User interaction gets Category, Type and Quantity

--- edu/ucalgary/ensf409/SupplyChainManager.java
+++ edu/ucalgary/ensf409/SupplyChainManager.java
@@ -71,16 +71,162 @@ public class SupplyChainManager{
 
         System.out.println("Welcome to the SupplyChainManager Program!");
         System.out.println("___________________________________________");
+        // play the menu
+        this.printCategories();
         // get the user input return an integer
-        userResponseInt=userCategorySelection();  
+        userResponseInt=getResponseAsInt(1,4,"category of object");  
         // decode the integer back to a string category
         category = decodeCategorySelection(userResponseInt);
+        // get the user input return an integer
+        userResponseInt=userTypeSelection(userResponseInt); 
+        // decode the integer response into a type
+        type = decodeTypeSelection(userResponseInt,category);
+        // now get the quantity of objects 
+        quantity = getResponseAsInt(1,1000,"quantity of items");
         //debug print
-        System.out.println("The user selected: " + category);
-
+        System.out.println("The user selected category: " + category);
+        //debug print
+        System.out.println("The user selected type: " + type);
+        //debug print
+        System.out.println("The user selected a quantity of: " + quantity);
+        
         return;
     }
 
+    public int userTypeSelection(int numberInput){
+        // start off with an imposible selection
+        // if the return is -1 then an error has occured
+        int userResponseValue=-1; 
+               
+        switch (numberInput){
+
+        case 1:
+            this.playChairMenu();
+            userResponseValue=this.getResponseAsInt(1,5,"type of Chair");
+            break;
+    
+        case 2:
+            this.playDeskMenu();
+            userResponseValue=this.getResponseAsInt(1,3,"type of Desk");
+            break;
+
+        case 3:
+            this.playFilingMenu();
+            userResponseValue=this.getResponseAsInt(1,3,"type of Filing Cabinet");
+            break;
+
+        case 4:
+            this.playLampMenu();
+            userResponseValue=this.getResponseAsInt(1,3,"type of Lamp");
+            break;
+        } 
+
+        return userResponseValue;
+    }
+
+
+    public String decodeTypeSelection(int selection, String category){
+        String type="";
+        switch (category){
+            case "CHAIR":
+            switch(selection){
+                case 1:
+                type="Task";
+                break;
+                case 2:
+                type="Mesh";
+                break;
+                case 3:
+                type="Executive";
+                break;
+                case 4:
+                type="Kneeling";
+                break;
+                case 5:
+                type="Ergonomic";
+                break;
+            }
+            break;
+            
+            case "DESK":
+            switch(selection){
+                case 1:
+                type="Traditional";
+                break;
+                case 2:
+                type="Adjustable";
+                break;
+                case 3:
+                type="Standing";
+                break;
+            }    
+            break;
+            
+            case "FILING":
+            switch(selection){
+                case 1:
+                type="Small";
+                break;
+                case 2:
+                type="Medium";
+                break;
+                case 3:
+                type="Large";
+                break;
+            }
+            break;
+            
+            case "LAMP":
+            switch(selection){
+                case 1:
+                type="Desk";
+                break;
+                case 2:
+                type="Swing Arm";
+                break;
+                case 3:
+                type="Study";
+                break;
+            }           
+            break;
+        }
+        return type;
+    }
+
+
+
+
+
+    public int getResponseAsInt(int lowerBound, int upperBound, String selectionStr){
+
+    int userResponseInt;
+    String userResponseStr="";
+    System.out.println("What " + selectionStr + " would you like?");
+    // get a line of keyboard input
+    userResponseStr=keyconsole.nextLine();
+    try{
+    userResponseInt = Integer.parseInt(userResponseStr); // try to convert to int
+    }catch(NumberFormatException e){
+        System.out.println(e.getMessage());
+        userResponseInt=-1;
+    }
+    
+    while ( userResponseInt < lowerBound || userResponseInt > upperBound) {
+        System.out.println("Error: Selection out of range, try again!");
+        userResponseStr=keyconsole.nextLine();
+        try{
+            // try to convert
+            userResponseInt = Integer.parseInt(userResponseStr); // try to convert to int
+            }catch(NumberFormatException e){
+                System.out.println(e.getMessage());
+                userResponseInt=-1; // stay in the loop
+            }
+    }
+    return userResponseInt;
+        
+    }
+
+    // depecated for code reuse in getResponseAsInt
     public int userCategorySelection(){ 
         int userResponseInt;
         String userResponseStr="";
@@ -92,7 +238,7 @@ public class SupplyChainManager{
     userResponseInt = Integer.parseInt(userResponseStr); // try to convert to int
     }catch(NumberFormatException e){
         System.out.println(e.getMessage());
-        userResponseInt=-1;
+        userResponseInt=-1;     
     }
     
     while ( userResponseInt < 1 || userResponseInt > 4) {
@@ -107,7 +253,8 @@ public class SupplyChainManager{
             }
     }
     return userResponseInt;
-}
+   
+    }
 
     public String decodeCategorySelection(int selection){
         String category="";
@@ -129,6 +276,7 @@ public class SupplyChainManager{
     }
 
 
+
     /**
      * print categories prints option choices for the 
      * categories from DB INVENTORY.  These should be constant
@@ -142,6 +290,42 @@ public class SupplyChainManager{
         System.out.println("Enter a number and press the RETURN key.");
         return;
     }
+
+    public void playChairMenu(){
+        System.out.println("1. Task ");
+        System.out.println("2. Mesh ");
+        System.out.println("3. Kneeling ");
+        System.out.println("4. Executive ");
+        System.out.println("5. Ergonomic ");
+        System.out.println("___________________________________________");
+        System.out.println("Enter a number and press the RETURN key.");
+    }
+
+    public void playDeskMenu(){
+        System.out.println("1. Traditional ");
+        System.out.println("2. Adjustable ");
+        System.out.println("3. Standing ");
+        System.out.println("___________________________________________");
+        System.out.println("Enter a number and press the RETURN key.");
+    }
+
+    public void playFilingMenu(){
+        System.out.println("1. Small ");
+        System.out.println("2. Medium ");
+        System.out.println("3. Large ");
+        System.out.println("___________________________________________");
+        System.out.println("Enter a number and press the RETURN key.");
+    }
+
+    public void playLampMenu(){
+        System.out.println("1. Desk ");
+        System.out.println("2. Swing Arm ");
+        System.out.println("3. Study ");
+        System.out.println("___________________________________________");
+        System.out.println("Enter a number and press the RETURN key.");
+    }
+
+
 
     /////////////////////////////////////////////////////////////////////
     /**
