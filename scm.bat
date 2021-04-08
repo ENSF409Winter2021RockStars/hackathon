65d05ea28878c584f10f8a56ee87de3763423521
mwpelletier <mwpellet@ucalgary.ca>
2021-04-08 00:57:36 -0600

    Added REM to test.bat and scm.bat to remove unwanted command line output,

--- scm.bat
+++ scm.bat
@@ -5,5 +5,5 @@ REM # Team: ENSF409 Group 48
 REM # Team Members: Zorondras Rodriguez, Jade Meggitt, Matthew Pelletier, Quinn Ledingham
 REM #########################################################################################
 
-## Run the Main Program
+REM ## Run the Main Program
 java -cp .;lib/mysql-connector-java-8.0.23.jar edu.ucalgary.ensf409.SupplyChainManager
